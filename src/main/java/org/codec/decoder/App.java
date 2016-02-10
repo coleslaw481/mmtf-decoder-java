ec017732cbe26a7bd129fa86f0390aee79ef6362
Anthony <anthony.bradley@rcsb.org>
2016-02-10 11:56:22 -0800

    Initial commit

---
+++ src/main/java/org/codec/decoder/App.java
@@ -0,0 +1,13 @@
+package org.codec.decoder;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
