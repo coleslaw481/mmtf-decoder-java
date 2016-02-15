6038482b9e52d37afb783c0700956daa549db0db
Anthony Bradley <abradley@Anthonys-MacBook-Air.local>
2016-02-15 15:51:14 -0800

    Update to be able to store encodings on the decodign side

---
+++ src/main/java/org/codec/dataholders/CodeHolders.java
@@ -0,0 +1,26 @@
+package org.codec.dataholders;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class CodeHolders {
+
+	// A map to indicate secondary structure information
+	public Map<String, Integer> dsspMap;
+	
+	public CodeHolders(){
+		Map<String, Integer> aMap = new HashMap<String, Integer>();
+		//		aMap.put(key, value);
+		aMap.put("pi Helix", 0);
+		aMap.put("Bend", 1);
+		aMap.put("alpha Helix",2);
+		aMap.put("Extended", 3);
+		aMap.put("3-10 Helix", 4);
+		aMap.put("Bridge", 5);
+		aMap.put("Turn", 6);
+		aMap.put("Coil",7);
+		aMap.put("NA", -1);
+		dsspMap = Collections.unmodifiableMap(aMap);
+	}
+}
