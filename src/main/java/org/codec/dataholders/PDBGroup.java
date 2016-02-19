275a2df2eace87794962cf8954f73da0996b4829
abradle <anthony.richard.bradley@gmail.com>
2016-02-19 14:08:05 -0800

    Update getter and setter to rename resName

--- src/main/java/org/codec/dataholders/PDBGroup.java
+++ src/main/java/org/codec/dataholders/PDBGroup.java
@@ -15,6 +15,22 @@ public class PDBGroup implements Serializable {
 	 */
 	private static final long serialVersionUID = 2880633780569899800L;
 	
+	
+	
+	// The name of the group (e.g. HIS)
+	private String groupName = new String();
+	// Whether the group is a HETATM record or not
+	private boolean hetFlag = false;
+	// A list of strings indicating the atominfo (Atom name and element name
+	private List<String> atomInfo = new ArrayList<String>();
+	// A list of strings indicating the bond orders
+	private List<Integer> bondOrders = new ArrayList<Integer>();
+	// A list of strings indicating the bond indices (e.g. 0,1 means there is bond between
+	// The 0th and 1st atoms in the group
+	private List<Integer> bondIndices = new ArrayList<Integer>();
+	// The atomic charges of these atoms
+	private List<Integer> atomCharges = new ArrayList<Integer>();
+	
 	// Generic getter and setter functions
 	public List<String> getAtomInfo() {
 		return atomInfo;
@@ -34,12 +50,6 @@ public class PDBGroup implements Serializable {
 	public void setBondIndices(List<Integer> bondIndices) {
 		this.bondIndices = bondIndices;
 	}
-	public String getResName() {
-		return groupName;
-	}
-	public void setResName(String resName) {
-		this.groupName = resName;
-	}
 	public boolean isHetFlag() {
 		return hetFlag;
 	}
@@ -53,18 +63,10 @@ public class PDBGroup implements Serializable {
 	public void setAtomCharges(List<Integer> atomCharges) {
 		this.atomCharges = atomCharges;
 	}
-	
-	// The name of the group (e.g. HIS)
-	private String groupName = new String();
-	// Whether the group is a HETATM record or not
-	private boolean hetFlag = false;
-	// A list of strings indicating the atominfo (Atom name and element name
-	private List<String> atomInfo = new ArrayList<String>();
-	// A list of strings indicating the bond orders
-	private List<Integer> bondOrders = new ArrayList<Integer>();
-	// A list of strings indicating the bond indices (e.g. 0,1 means there is bond between
-	// The 0th and 1st atoms in the group
-	private List<Integer> bondIndices = new ArrayList<Integer>();
-	// The atomic charges of these atoms
-	private List<Integer> atomCharges = new ArrayList<Integer>();
+	public String getGroupName() {
+		return groupName;
+	}
+	public void setGroupName(String groupName) {
+		this.groupName = groupName;
+	}
 }
