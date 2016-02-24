e20eef06d5e9b26d6f7195e76ca00bf1b886aca0
abradle <anthony.richard.bradley@gmail.com>
2016-02-23 17:16:32 -0800

    Update to example script

--- src/main/java/org/codec/examples/ExampleScript.java
+++ src/main/java/org/codec/examples/ExampleScript.java
@@ -10,12 +10,9 @@ public class ExampleScript {
 	public static void main(String[] args) throws FileNotFoundException, IOException {
 		// Set the PDB_CACHE_DIR path 
 		System.setProperty("PDB_CACHE_DIR","/Users/anthony/PDB_CACHE");
-
 		GetBioJavaStructs gbjs = new GetBioJavaStructs();
 		Structure thatStruct = gbjs.getFromUrl("4cup");
-		
 		System.out.println(thatStruct.getChains());
-
 	}
 
 }
