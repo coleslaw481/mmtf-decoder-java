c23171f5e5b89f55f527fbc85bc2742b01cd0926
abradle <anthony.richard.bradley@gmail.com>
2016-03-01 16:21:39 -0800

    New bean to store data from calpha

---
+++ src/main/java/org/codec/dataholders/CalphaAlignBean.java
@@ -0,0 +1,53 @@
+package org.codec.dataholders;
+
+import java.util.List;
+
+import javax.vecmath.Point3d;
+
+
+/**
+ * A class to hold c-alpha coords
+ * @author anthony
+ *
+ */
+public class CalphaAlignBean {
+
+	private String pdbId;
+	private String chainId;
+	private String polymerType;
+	private char[] sequence;
+	private List<Point3d> coordList;
+
+	
+	public String getPdbId() {
+		return pdbId;
+	}
+	public void setPdbId(String pdbId) {
+		this.pdbId = pdbId;
+	}
+	public String getChainId() {
+		return chainId;
+	}
+	public void setChainId(String chainId) {
+		this.chainId = chainId;
+	}
+	public String getPolymerType() {
+		return polymerType;
+	}
+	public void setPolymerType(String polymerType) {
+		this.polymerType = polymerType;
+	}
+	public char[] getSequence() {
+		return sequence;
+	}
+	public void setSequence(char[] sequence) {
+		this.sequence = sequence;
+	}
+	public List<Point3d> getCoordList() {
+		return coordList;
+	}
+	public void setCoordList(List<Point3d> coordList) {
+		this.coordList = coordList;
+	}
+	
+}
